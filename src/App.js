d1e2a97932ddfb851db8f393dc563f12058fb243
thommie-wallin <thommie.wallin@chasacademy.se>
2021-06-19 21:06:11 +0200

    create geolocation feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+function App() {  
+  const [location, setLocation] = useState(null)
+
+  useEffect(() => {
+    if('geolocation' in navigator) {
+      if (!location) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          setLocation([position.coords.latitude, position.coords.longitude]);
+        });
+      }
+    } else {
+      /* geolocation IS NOT available */
+    }
+  })
+
+  console.log(location)
+
   return (
     <div>
-    
+      <h1>{location}</h1>
     </div>
   );
 }
