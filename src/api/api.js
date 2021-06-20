2f7acfa2d5af6c0768a13e4fdf5d76c8d6c7a163
thommie-wallin <thommie.wallin@chasacademy.se>
2021-06-20 19:16:53 +0200

    add API key to .env

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,13 @@ import { useGeolocation } from './geolocation';
 
 export const Api = () => {
   const {latitude, longitude, error} = useGeolocation();
-  
+  const {weatherData, setWeatherData} = useState();
+  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+
+  useEffect(() => {
+    
+console.log(process.env.REACT_APP_WEATHER_API_KEY)
+  }, [])
 
   return (
     <div>
