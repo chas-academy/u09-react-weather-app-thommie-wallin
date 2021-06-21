df0f13c8a23a08774281d53dbc7640a6eb1b9809
thommie-wallin <thommie.wallin@chasacademy.se>
2021-06-21 17:36:29 +0200

    create API call and send to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import './api/api.js';
-import {useGeolocation} from './api/geolocation';
 import { Api } from './api/api.js';
+import { useWeatherData } from './api/api.js';
 
 function App() {  
+  const {weatherData, setWeatherData} = useWeatherData();
 
   useEffect(() => {
+    
+    console.log(weatherData)
+  }, [weatherData])
 
-  }, [])
+  // const getWeatherData = () => {
+  //   setWeatherData()
+  // }
 
-  // console.log(location)
+  // const fetchTask = () => {
+  //   const res = useWeatherData()
+  //   console.log(res)
+  //   return res;
+  // }
 
   return (
     <div>
       {/* {location && <h1>{location}</h1>} */}
       
-      <Api />
+      {/* <Api /> */}
     </div>
   );
 }
+
+// function fetchWeatherData(props) {
+//   const {weatherData, setWeatherData} = useWeatherData();
+//   console.log(weatherData)
+// }
 
 export default App;
