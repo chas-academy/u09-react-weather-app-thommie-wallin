4bf17a0f4458968c1a1ca65c9696d150a06873ea
thommie-wallin <thommie.wallin@chasacademy.se>
2021-06-22 15:31:52 +0200

    remove unused code in App.js

--- src/App.js
+++ src/App.js
@@ -11,28 +11,28 @@ function App() {
     console.log(weatherData)
   }, [weatherData])
 
-  // const getWeatherData = () => {
-  //   setWeatherData()
-  // }
-
-  // const fetchTask = () => {
-  //   const res = useWeatherData()
-  //   console.log(res)
-  //   return res;
-  // }
-
   return (
     <div>
-      {/* {location && <h1>{location}</h1>} */}
       
-      {/* <Api /> */}
     </div>
   );
 }
 
-// function fetchWeatherData(props) {
-//   const {weatherData, setWeatherData} = useWeatherData();
-//   console.log(weatherData)
-// }
-
 export default App;
+
+
+//TODO Som användare ska man kunna göra följande:
+
+// - Kunna se följande väderförhållanden för sin nuvarande position:
+//? Temperatur
+//? Vindstyrka
+//? Luftfuktighet
+//? Soluppgång och nedgång (klockslag)
+//? Välja mellan Farenheit och Celsius
+// - Kunna få en väderleksprognos för väderförhållanden (enligt ovan) 5 dagar framåt 
+//? Kort översikt för veckan
+//? Var tredje timme för nuvarande dygn
+//? 5-dagarsprognos
+//* Nyttja ett väder-API, t.ex. SMHI, YR.NO, OpenWeatherMaps
+//* Nyttja positionering via geolocation i webbläsaren
+//? Design/färg & form baserad på weather.com (Länkar till en externa sida.)Länkar till en externa sida. eller liknande applikationer/appar
