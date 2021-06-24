781dc0295b85830c6d00f9c4eb47950fc6fa3ac5
thommie-wallin <thommie.wallin@chasacademy.se>
2021-06-24 15:27:44 +0200

    create WeekForecast-component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useWeatherData } from './api/api.js';
 import Today from './components/Today';
 import WeekOverview from './components/WeekOverview';
 import Hourly from './components/Hourly';
+import WeekForecast from './components/WeekForecast';
 
 function App() {  
   const {weatherData} = useWeatherData();
@@ -20,6 +21,7 @@ function App() {
       {Object.keys(weatherData).length > 0 && <Today weatherData={weatherData} isTempUnit={isTempUnit} />}
       {Object.keys(weatherData).length > 0 && <WeekOverview weatherData={weatherData} isTempUnit={isTempUnit} />}
       {Object.keys(weatherData).length > 0 && <Hourly weatherData={weatherData} isTempUnit={isTempUnit} />}
+      {Object.keys(weatherData).length > 0 && <WeekForecast weatherData={weatherData} isTempUnit={isTempUnit} />}
     </div>
   );
 }
@@ -37,8 +39,8 @@ export default App;
 //? Välja mellan Farenheit och Celsius
 // - Kunna få en väderleksprognos för väderförhållanden (enligt ovan) 5 dagar framåt 
 //* Kort översikt för veckan
-//? Var tredje timme för nuvarande dygn
-//? 5-dagarsprognos
+//* Var tredje timme för nuvarande dygn
+//* 5-dagarsprognos
 //* Nyttja ett väder-API, t.ex. SMHI, YR.NO, OpenWeatherMaps
 //* Nyttja positionering via geolocation i webbläsaren
 //? Design/färg & form baserad på weather.com (Länkar till en externa sida.)Länkar till en externa sida. eller liknande applikationer/appar
